ac26e66de6abdb0b2b0bdd38720ba3760bdd077e
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-30 12:45:18 +0500

    updated with main screen banner

--- news/lib/screens/home.dart
+++ news/lib/screens/home.dart
@@ -16,8 +16,9 @@ class _homeViewState extends State<homeView> {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50),
         child: Padding(
-          padding: const EdgeInsets.only(top: 40, left: 20),
+          padding: const EdgeInsets.only(top: 40, left: 20, right: 10),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
                 width: 50,
@@ -29,10 +30,61 @@ class _homeViewState extends State<homeView> {
                   ),
                 ),
                 child: IconButton(onPressed: () {}, icon: const Icon(Iconsax.arrow_left)),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Skip",
+                  style: TextStyle(
+                    fontFamily: "MontR",
+                    color: Colors.red,
+                  ),
+                ),
               )
             ],
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Container(
+                width: double.infinity,
+                height: 450,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      child: Image.asset(
+                        "assets/mainImage.png",
+                        fit: BoxFit.cover,
+                        height: 600,
+                      ),
+                    ),
+                    const Positioned(
+                      bottom: 50,
+                      left: 20,
+                      child: Text(
+                        "Sign In \nOptions.",
+                        style: TextStyle(
+                          fontFamily: "MontB",
+                          color: Colors.white,
+                          fontSize: 40,
+                          height: 1,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            child: ElevatedButton(onPressed: (){}, child: text),
+          ),
+        ],
       ),
     );
   }
