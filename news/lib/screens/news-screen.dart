311fe64e4f9f4597f8bfe7ba8c6e30f33a1f3f0c
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-30 14:31:02 +0500

    updated with banner image

--- news/lib/screens/news-screen.dart
+++ news/lib/screens/news-screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:news/API/api-call.dart';
 
 class newsScreen extends StatefulWidget {
   const newsScreen({super.key});
@@ -45,6 +46,68 @@ class _newsScreenState extends State<newsScreen> {
               ],
             ),
           )
+        ],
+      ),
+      body: Column(
+        children: [
+          FutureBuilder(
+              future: getBusinessData(),
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return Expanded(
+                    child: Container(
+                      child: ListView.builder(
+                        itemCount: 1,
+                        itemBuilder: (context, index) {
+                          var data = businessData[1];
+                          return Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.all(Radius.circular(40)),
+                              child: Container(
+                                color: Colors.amber,
+                                width: double.infinity,
+                                height: 400,
+                                child: Stack(
+                                  children: [
+                                    Positioned(
+                                      child: Image(
+                                        height: double.infinity,
+                                        fit: BoxFit.fitHeight,
+                                        image: NetworkImage(
+                                          data.urlToImage ?? "https://i.sstatic.net/y9DpT.jpg",
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                        top: 20,
+                                        right: 20,
+                                        child: ClipRRect(
+                                          borderRadius: const BorderRadius.all(Radius.circular(10)),
+                                          child: Container(
+                                            color: Colors.red,
+                                            height: 50,
+                                            width: 50,
+                                            child: IconButton(
+                                                onPressed: () {},
+                                                icon: const Icon(
+                                                  Iconsax.save_2,
+                                                  color: Colors.white,
+                                                )),
+                                          ),
+                                        ))
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  );
+                }
+                return const CircularProgressIndicator();
+              }),
         ],
       ),
     );
